fab37e15beb3343aa0d2b00bd2cece6192c1bad7
Mate Kovacs <m0kova01@louisville.edu>
2020-06-08 22:11:28 -0400

    api

---
+++ back-end/LeagueAppAPI/Models/SummonerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeagueAppAPI.Models
+{
+    public class SummonerModel
+    {
+        public int SummonerID { get; set; }
+        public string SummonerName { get; set; }
+    }
+}
