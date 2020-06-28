37ecb2b8679af90fe647b5391ed56fd647c546d3
Matyi Kovacs <m0kova01@louisville.edu>
2020-06-28 14:25:36 -0400

    added participant model

---
+++ front-end/src/app/shared/models/participant-model.ts
@@ -0,0 +1,9 @@
+import StatsModel from './stasts-model';
+
+export default class ParticipantModel {
+    participantID: number;
+    summonerName: string;
+    championID: number;
+    teamID: number;
+    stats: StatsModel;
+}
